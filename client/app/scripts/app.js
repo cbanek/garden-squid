6032b0f9b333ae14a6026f290bb5f50ba45294b6
Christine Banek <cbanek@gmail.com>
2016-07-28 22:59:55 -0700

    Add in angular flot and make a graph with test data.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'angular-flot'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+  $scope.dataset = [{ data: [], yaxis: 1, label: 'sin' }];
+  $scope.options = {
+    legend: {
+      container: '#legend',
+      show: true
+    }
+  };
+
+  for (var i = 0; i < 14; i += 0.5) {
+    $scope.dataset[0].data.push([i, Math.sin(i)]);
+  }
+
   });
